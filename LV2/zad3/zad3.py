39991f9543354254764ae4439561c48e61aa4855
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2023-04-05 09:43:02 +0200

    Add files via upload

---
+++ LV2/zad3/zad3.py
@@ -0,0 +1,54 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+img = plt.imread('C:\\Users\\student\\Desktop\\LV2\\zad3\\tiger.png')
+
+plt.figure(1)
+plt.imshow(img, cmap='gray', vmin = 0, vmax = 255)
+plt.show()
+
+#povecanje svjetline
+svjetlina = 2
+psvjetlina_img = img * svjetlina
+plt.figure(2)
+plt.imshow(psvjetlina_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+#rotacija
+rotirana_img = np.rot90(img, k = 3)
+plt.figure(3)
+plt.imshow(rotirana_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+#zrcaljenje
+mirror_img = np.fliplr(img)
+
+plt.figure(4)
+plt.imshow(mirror_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+#smanjenje rezolucije
+# svaki sesti element dohvati smanjujemo rezoluciju za 6
+smanjena_rez = img[::6,::6]
+
+plt.figure(5)
+plt.imshow(smanjena_rez, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+# promjena slike
+redovi = img.shape[0]
+stupci = img.shape[1]
+dg = stupci//4
+gg = stupci//2
+
+pr_img = img.copy()
+for i in range(redovi):
+    for j in range(stupci):
+        if (j < dg or j > gg): 
+            pr_img[i][j] = 0
+
+plt.figure(6)
+plt.imshow(pr_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
